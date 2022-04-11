f374a13238526e5faf7c4d1592b4ab0b6ad25bcc
DPS7991 <danielpetersalawu@gmail.com>
2022-04-11 09:50:02 -0500

    improved security

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-tsparticles';
-import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 import './App.css';
-
-const app = new Clarifai.App({
-	apiKey: '4d2cd68022954362983698b7fadca514',
-});
 
 const particlesOptions = {
 	fpsLimit: 30,
@@ -145,8 +140,14 @@ class App extends Component {
 
 	onButtonSubmit = () => {
 		this.setState({ imageUrl: this.state.input });
-		app.models
-			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+		fetch('http://localhost:3000/imageurl', {
+			method: 'post',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				input: this.state.input,
+			}),
+		})
+			.then((response) => response.json())
 			.then((response) => {
 				if (response) {
 					fetch('http://localhost:3000/image', {
